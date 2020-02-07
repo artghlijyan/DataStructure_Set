d833ea65b452a86e18d4912d458519fb5806e25a
Art <deckuser@hotmail.com>
2020-02-07 17:11:03 +0400

    Add project files.

---
+++ DataStructure_Set/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DataStructure_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4 };
+            int[] arr1 = { 1, 2, 3, 4, 5, 6 };
+
+            Set<int> instance1 = new Set<int>(arr);
+            Set<int> instance2 = new Set<int>(arr1);
+
+            bool res = instance1.IsSubSet(instance2);
+
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ DataStructure_Set/Set.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructure_Set
+{
+    class Set<T> : IEnumerable<T> where T : IComparable<T>
+    {
+        private readonly List<T> _items = new List<T>();
+
+        public int Count { get => this._items.Count; }
+
+        public Set()
+        {
+
+        }
+
+        public Set(IEnumerable<T> items)
+        {
+            AddRange(items);
+        }
+
+        public Set<T> Union(Set<T> other)
+        {
+            Set<T> result = new Set<T>(this._items);
+
+            foreach (T item in other._items)
+            {
+                if (!result.Contain(item))
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+
+        public Set<T> Intersection(Set<T> other)
+        {
+            Set<T> result = new Set<T>();
+
+            foreach (T item in other)
+            {
+                if (_items.Contains(item))
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+
+        public Set<T> Difference(Set<T> other)
+        {
+            Set<T> result = new Set<T>(this);
+
+            foreach (T item in other)
+            {
+                if (result.Contain(item))
+                {
+                    result.Remove(item);
+                }
+            }
+
+            return result;
+        }
+
+        public Set<T> SymetricDifference(Set<T> other)
+        {
+            Set<T> result = this.Union(this);
+
+            foreach (T item in this)
+            {
+                if (other.Contain(item) && result.Contain(item))
+                {
+                    result.Remove(item);
+                    other.Remove(item);
+                }
+            }
+
+            result.AddRange(other);
+            return result;
+        }
+
+        public bool IsSubSet(Set<T> other)
+        {
+            int count = 0;
+
+            foreach (var item in other)
+            {
+                if (this.Contain(item))
+                {
+                    count++;
+                }
+            }
+
+            return count == this.Count ? true : false;
+        }
+
+        public void Add(T item)
+        {
+            if (this.Contain(item))
+            {
+                throw new InvalidOperationException("The value already exists in set");
+            }
+
+            this._items.Add(item);
+        }
+
+        internal bool Remove(T item)
+        {
+            return _items.Remove(item);
+        }
+
+        internal void AddRange(IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                Add(item);
+            }
+        }
+
+        public bool Contain(T item)
+        {
+            return this._items.Contains(item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this._items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
